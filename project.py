0901e00f6c4f358ab15bd10c69c50c3b4c4ecda9
Afsana Khan <58783192+Afsana2910@users.noreply.github.com>
2019-12-25 00:48:57 +0200

    Update project.py

--- project.py
+++ project.py
@@ -4,14 +4,14 @@ import numpy as np
 
 
 
-def lcs(X, Y, m, n): # m and n are the lengths of strings X and Y respectively
+def LCS_function(X, Y, m, n): # m and n are the lengths of strings X and Y respectively
   
     if m == 0 or n == 0: 
        return 0; 
     elif X[m-1] == Y[n-1]: 
-       return 1 + lcs(X, Y, m-1, n-1); 
+       return 1 + LCS_function(X, Y, m-1, n-1); 
     else: 
-       return max(lcs(X, Y, m, n-1), lcs(X, Y, m-1, n)); # Returns the length of the longest common sequence
+       return max(LCS_function(X, Y, m, n-1), LCS_function(X, Y, m-1, n)); # Returns the length of the longest common sequence
 
 
 
