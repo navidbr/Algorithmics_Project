e8301dedb306299be1c4ac33f6dc58cf39a38343
Navid Bamdad Roshan <nbamdadroshan@gmail.com>
2020-01-13 01:02:38 +0200

    Update project.py

--- project.py
+++ project.py
@@ -81,19 +81,19 @@ class Genetic_Class:
     
     
     
-    def mutation(self, A):
+    def mutation(self, A):                      # 50% of each gene gets mutated
         result = np.empty([len(A)], dtype=str)
         for j in range(len(A)):
             result[j] = A[j]
-        for i in range(int(self.gene_length * 0.1)):
+        for i in range(int(self.gene_length * 0.5)):
             result[np.random.randint(self.gene_length-1)] = np.random.choice(['A', 'C', 'G', 'T'])
         return ''.join(result)
     
 
     
     def squared_average(self,A):
-        temp = np.sum(np.power(A,2))/A.shape[0]
-        return np.sqrt(temp)
+        temp = np.sqrt(np.sum(np.power(A,2)))/A.shape[0]
+        return temp
         
 
 
@@ -102,46 +102,54 @@ class Genetic_Class:
             generation_number = self.max_generation - self.generation
         if self.max_generation - self.generation < generation_number:
             raise Exception('generation_number parameter of next_generation function is more than allowed.')
+        scores = np.empty([2*self.population_number, self.reference_gene_number], dtype=int)
+        squared_average_score = np.empty([2*self.population_number], dtype=float)
         for rep in range(generation_number):
             self.generation += 1
             perm = np.random.permutation(np.array(range(self.population_number)))
-            for i in range(0, int(0.8*perm.shape[0]), 2):      # 0.8 of population go to cross over
+            for i in range(0, int(0.7*perm.shape[0]), 2):      # 0.7 of population go to cross over
                 ch1, ch2 = self.cross_over(self.population[perm[i]], self.population[perm[i+1]])
                 self.population[i+self.population_number] = ch1
                 self.population[i+self.population_number+1] = ch2
                 
-            for i in range(int(0.8*perm.shape[0]), perm.shape[0]):
+            for i in range(int(0.7*perm.shape[0]), perm.shape[0]):  # 0.3 of population go to mutation
                 self.population[i+self.population_number] = self.mutation(self.population[perm[i]])
                 
-            scores = np.empty([2*self.population_number, self.reference_gene_number], dtype=int)
-            squared_average_score = np.empty([2*self.population_number], dtype=float)
-            for i in range(2*self.population_number):
+            if self.generation == 0:
+                for i in range(self.population_number):
+                    for j in range(self.reference_gene_number):
+                        scores[i,j] = LCS_function(str(self.population[i]), str(self.reference_genes[j]))
+                    squared_average_score[i] = self.squared_average(scores[i])
+
+            for i in range(self.population_number, 2*self.population_number):
                 for j in range(self.reference_gene_number):
                     scores[i,j] = LCS_function(str(self.population[i]), str(self.reference_genes[j]))
-            for i in range(2*self.population_number):
                 squared_average_score[i] = self.squared_average(scores[i])
+                
             scored_population = np.empty([2*self.population_number, 3], dtype=object)
             scored_population.T[0] = squared_average_score
             scored_population.T[1] = self.population
             scored_population.T[2] = np.array(range(2*self.population_number))
-            scored_population = np.sort(scored_population,0)
+            scored_population = scored_population[np.argsort(scored_population[:,0])]
             scored_population = np.flip(scored_population,0)
             self.population = scored_population[:,1]
             order = scored_population[:,2]                    #indexes of sorted population
+            temp_scores = np.empty([2*self.population_number, self.reference_gene_number], dtype=int)
+            for i in range(2*self.population_number):
+                temp_scores[i] = scores[order[i]]
+            scores = temp_scores
+            squared_average_score = scored_population[:,0]
             del scored_population
-        population_score = np.empty([self.population_number, self.reference_gene_number], dtype=int)
-        for i in range(self.population_number):
-            population_score[i] = scores[order[i]]
+            del temp_scores
+        population_score = scores[0:self.population_number]
         
         return np.array((population_score/self.gene_length)*100, dtype=int)         # reurns the percentage of similarity of each gene to each reference gene.
 
-GC = Genetic_Class(refrence_gene_number=10, reference_genes=-1, population_number=100, gene_length=50, max_generation=20)   #Initialization of genetic algorithm
-results1 = GC.next_generation(7)     # run 7 generations and return the result after 7 generations
+GC = Genetic_Class(refrence_gene_number=5, reference_genes=-1, population_number=100, gene_length=50, max_generation=50)   #Initialization of genetic algorithm
+results1 = GC.next_generation(6)     # run 7 generations and return the result after 7 generations
 # result is a matrix of size (population_number*refrence_gene_number), that contains the percentage of similarity of each gene of population to each reference gene.
-results2 = GC.next_generation(4)    # run 4 more generations and return the result after 4 more generations (returns the result of 7 + 4 = 11th generation)
+results2 = GC.next_generation(3)    # run 4 more generations and return the result after 4 more generations (returns the result of 7 + 4 = 11th generation)
 
 results3 = GC.next_generation('max')    # run remaining generations and return the result. (generation 12 to max generation)
 
 print(results3)
-        
-        
